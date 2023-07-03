f0e0b91c9e79520700300a97b33b9af72efaaac1
alvyynm <alvyynm@gmail.com>
2023-07-03 06:27:14 +0300

    remove test file from staging area

--- crawl.test.js
+++
@@ -1,9 +0,0 @@
-const { normalizeURL } = require("./crawl.js");
-const { test, expect } = require("@jest/globals");
-
-test("normalizeURL strip protocol from URL", () => {
-  const input = "https://blog.alvinwanjala.com/posts/2014";
-  const actual = normalizeURL(input);
-  const expected = "blog.alvinwanjala.com/posts/2014";
-  expect(actual).toEqual(expected);
-});
