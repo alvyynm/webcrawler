c5dad988a1baadcddff9215dcae0d96248f34bd4
alvyynm <alvyynm@gmail.com>
2023-07-03 06:57:08 +0300

    add tests for removing capitilization from url

--- crawl.test.js
+++ crawl.test.js
@@ -14,3 +14,10 @@ test("normalizeURL strip trailing slash from URL", () => {
   const expected = "blog.alvinwanjala.com/posts/2014";
   expect(actual).toEqual(expected);
 });
+
+test("normalizeURL remove capitalization", () => {
+  const input = "https://blog.ALvinwanjala.com/posts/2014";
+  const actual = normalizeURL(input);
+  const expected = "blog.alvinwanjala.com/posts/2014";
+  expect(actual).toEqual(expected);
+});
