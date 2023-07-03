a2ecdc0b060a9eec1a00df6fb74456edb2343d06
alvyynm <alvyynm@gmail.com>
2023-07-03 06:58:59 +0300

    remove toLowerCase and use built-in functionality from URL obj

--- crawl.js
+++ crawl.js
@@ -1,6 +1,6 @@
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
-  let hostPath = `${urlObj.hostname}${urlObj.pathname}`.toLowerCase();
+  let hostPath = `${urlObj.hostname}${urlObj.pathname}`;
   if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
     return hostPath.slice(0, -1);
   }
