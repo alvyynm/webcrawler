c20700243e1e0279810b6a9e427eccafa9c200c5
alvyynm <alvyynm@gmail.com>
2023-07-04 11:28:52 +0300

    create main function for running the program

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const { crawlPage } = require("./crawl.js");
+
+function main() {
+  if (process.argv.length < 3) {
+    console.log("no website provided");
+    process.exit(1);
+  } else if (process.argv.length > 3) {
+    console.log("too many arguments");
+    process.exit(1);
+  } else {
+    const baseURL = process.argv[2];
+    console.log(`starting to crawl ${baseURL}`);
+    crawlPage(baseURL);
+  }
+}
+
+main();
